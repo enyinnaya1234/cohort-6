93e72bfea6ea3ca478e2f98655dfbb3892f5b675
enyinnaya1234 <chemicalnilton@gmail.com>
2025-02-19 01:07:33 +0100

    feat: add crowdfunding contract

--- submissions/Assignment3/enyinnaya1234/ERC20.sol
+++ submissions/Assignment3/enyinnaya1234/ERC20.sol
@@ -2,25 +2,31 @@
 
 pragma solidity ^0.8.24;
 
-import "./IERC20.sol";
 
-contract ERC20 is IERC20{
+contract ERC20 {
+    error FunctionDoesNotExist();
     string public name;
     string public symbol;
-    uint8 public decimals;
+    uint8 public decimals ;
     uint256 public totalSupply;
 
-    mapping(address => uint256) public balance;
+    mapping(address => uint256) public balanceOf;
     //mapping(address)
 
     event Transfer(address indexed _from, address indexed _to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
- constructor(string memory _name, string memory _symbol, uint8 _decimals){
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
+ constructor(uint _totalSupply, string memory _name, string memory _symbol, uint8 _decimals){
+   _totalSupply = totalSupply;
+   _name = name;
+   _symbol = symbol;
+   _decimals = decimals;
  } 
+
+    receive() external payable{}
+    fallback() external payable{
+        revert FunctionDoesNotExist();
+    }
 
  function transfer(address recipient, uint256 amount) external returns (bool){
     require(balanceOf[msg.sender] >= amount, "Insufficient funds");
@@ -28,6 +34,7 @@ contract ERC20 is IERC20{
     balanceOf[recipient] += amount;
 
     emit Transfer(msg.sender, recipient, amount);
+  return true;
  } 
 
  function mint(address _to, uint256 _amount) external{
